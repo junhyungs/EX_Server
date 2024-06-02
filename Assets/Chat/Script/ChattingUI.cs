5ad15aef454f3e738de1576724733a877811fd53
junHyung <jin05003@naver.com>
2024-06-03 01:25:25 +0900

    [Update]ChattingUI
    ChattingUI 업데이트
    커맨드라는 어트리뷰트를 이용해 클라에서 서버로 특정 기능 수행을 요청하는 메소드 작성, 서버 사이드에서 모든 클라에게 특정 함수를 실행할 수 있도록 ClientRpc 어트리뷰트를 사용하는 메소드 작성, UI처리 메소드 작성

--- Assets/Chat/Script/ChattingUI.cs
+++ Assets/Chat/Script/ChattingUI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Mirror;
 using UnityEngine.UI;
 using System.Runtime.InteropServices;
+using Mirror.Examples.Chat;
 
 public class ChattingUI : NetworkBehaviour
 {
@@ -27,6 +28,75 @@ public class ChattingUI : NetworkBehaviour
     }
     public override void OnStartClient()
     {
-        
+        Text_ChatHistroy.text = string.Empty;
     }
+    [Command(requiresAuthority = false)]//커맨드라는 어트리뷰트를 이용해 클라에서 서버로 특정 기능수행을 요청한다.
+    private void CommandSendMsg(string msg, NetworkConnectionToClient sender = null)
+    {
+        if (!m_connectedNameDic.ContainsKey(sender))
+        {
+            ChatUser player = sender.identity.GetComponent<ChatUser>();
+            string playerName = player.PlayerName;
+            m_connectedNameDic.Add(sender, playerName);
+        }
+
+        if (!string.IsNullOrWhiteSpace(msg))
+        {
+            var senderName = m_connectedNameDic[sender];
+            OnRecvMessge(senderName, msg.Trim());
+        }
+    }
+    [ClientRpc]//서버사이드에서 모든 클라이언트에게 특정 함수를 실행시킬 수 있도록 클라이언트RPC를 붙인다.
+    private void OnRecvMessge(string senderName, string msg)
+    {// 전송자와 현재 플레이어의 이름 비교 후 메세지 포매팅(색)
+        string formatedMsg = (senderName == m_LocalPlayerName) ?
+            $"<color = red>{senderName}:</color>{msg}" :
+            $"<color = blue>{senderName}:</color>{msg}";
+
+        AppendMessage(formatedMsg);
+    }
+
+    //UI----------------------------------------------------------
+    private void AppendMessage(string msg)
+    {//텍스트에 채팅 내용을 넣고 줄바꿈
+        StartCoroutine(AppendAndScroll(msg));
+    }
+
+    private IEnumerator AppendAndScroll(string msg)
+    {
+        Text_ChatHistroy.text += msg + "\n";
+
+        yield return null;
+        yield return null;
+
+        ScrollBar_Chat.value = 0;
+    }
+    //UI--------------------------------------------------------------
+    public void OnClick_SendMsg()
+    {
+        var currentChatMsg = Input_ChatMsg.text;
+        if (!string.IsNullOrWhiteSpace(currentChatMsg))
+        {
+            CommandSendMsg(currentChatMsg.Trim());
+        }
+    }
+    public void OnClick_Exit()
+    {
+        NetworkManager.singleton.StopHost();
+    }
+    public void OnValueChanged_ToggleButton(string input)
+    {
+        Btn_Send.interactable = !string.IsNullOrWhiteSpace(input);
+    }
+
+    public void OnEndEdit_SendMsg(string input)
+    {
+        if(Input.GetKeyDown(KeyCode.Return)
+            || Input.GetKeyDown(KeyCode.KeypadEnter)
+            || Input.GetButtonDown("Submit"))
+        {
+            OnClick_SendMsg();
+        }
+    }
+
 }
